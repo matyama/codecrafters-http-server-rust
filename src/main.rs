642d84975fa32cef291d40f231aaf4a921e8ece8
Martin Matyášek <martin.matyasek@gmail.com>
2024-06-04 15:05:15 +0200

    Switch to Tokio

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,26 @@
-use std::net::TcpListener;
+use anyhow::{Context as _, Result};
+use tokio::net::{TcpListener, TcpStream};
 
-fn main() {
-    let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
+async fn handle_connection(_stream: TcpStream) -> Result<()> {
+    println!("handled new connection");
+    Ok(())
+}
 
-    for stream in listener.incoming() {
-        match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
-            }
-            Err(e) => {
-                println!("error: {}", e);
+#[tokio::main]
+async fn main() -> Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:4221")
+        .await
+        .context("bind TCP listener")?;
+
+    loop {
+        tokio::select! {
+            stream = listener.accept() => match stream {
+                Ok((stream, addr)) => {
+                    if let Err(error) = tokio::spawn(handle_connection(stream)).await {
+                        eprintln!("connection {addr} failed with {error}");
+                    }
+                }
+                Err(error) => eprintln!("cannot get client: {error}"),
             }
         }
     }
